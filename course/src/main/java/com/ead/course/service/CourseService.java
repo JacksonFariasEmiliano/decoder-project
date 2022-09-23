684982b4006aac267b289362c66bc0a494e5eabb
Jackson Alves <jackson.emiliano15@gmail.com>
2022-09-23 19:47:41 -0300

    delation in cascade

--- course/src/main/java/com/ead/course/service/CourseService.java
+++ course/src/main/java/com/ead/course/service/CourseService.java
@@ -1,7 +1,9 @@
 package com.ead.course.service;
 
+import com.ead.course.model.CourseModel;
 import org.springframework.stereotype.Service;
 
 
 public interface CourseService {
+    void delete (CourseModel courseModel);
 }
--- course/src/main/java/com/ead/course/service/ModuleService.java
+++ course/src/main/java/com/ead/course/service/ModuleService.java
@@ -1,4 +1,8 @@
 package com.ead.course.service;
 
+import com.ead.course.model.ModuleModel;
+
 public interface ModuleService {
+
+    void delete(ModuleModel moduleModel);
 }
