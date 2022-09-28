eac480efaf84fdd40e4e8f6e9b13a172d1783761
Jackson Alves <jackson.emiliano15@gmail.com>
2022-09-28 20:58:58 -0300

    lessons restful

--- course/src/main/java/com/ead/course/service/LessonService.java
+++ course/src/main/java/com/ead/course/service/LessonService.java
@@ -1,4 +1,18 @@
 package com.ead.course.service;
 
+import com.ead.course.model.LessonModel;
+import com.ead.course.model.ModuleModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LessonService {
+    LessonModel save(LessonModel lessonModel);
+
+    Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId);
+
+    void delete(LessonModel lessonModel);
+
+    List<LessonModel> findAllByModule(UUID moduleId);
 }
--- course/src/main/java/com/ead/course/service/impl/LessonServiceImpl.java
+++ course/src/main/java/com/ead/course/service/impl/LessonServiceImpl.java
@@ -1,13 +1,40 @@
 package com.ead.course.service.impl;
 
+import com.ead.course.model.LessonModel;
+import com.ead.course.model.ModuleModel;
 import com.ead.course.repository.LessonRepository;
 import com.ead.course.service.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class LessonServiceImpl implements LessonService {
 
     @Autowired
     LessonRepository repository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return repository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        return repository.findLessonIntoModule(moduleId, lessonId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+       repository.delete(lessonModel);
+    }
+
+    @Override
+    public List<LessonModel> findAllByModule(UUID moduleId) {
+        return repository.findAllLessonsIntoModule(moduleId);
+    }
+
 }
