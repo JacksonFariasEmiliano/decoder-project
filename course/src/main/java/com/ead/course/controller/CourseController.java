e079de88aa690a2e222bf3c6239aa6390c01777f
Jackson Alves <jackson.emiliano15@gmail.com>
2022-09-28 13:38:10 -0300

    validation module

--- course/src/main/java/com/ead/course/controller/CourseController.java
+++ course/src/main/java/com/ead/course/controller/CourseController.java
@@ -36,7 +36,7 @@ public class CourseController {
     @DeleteMapping("/{courseId}")
     public ResponseEntity<Object> deleteCourse(@PathVariable(value = "courseId")UUID courseId){
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
-        if (!courseModelOptional.isEmpty()){
+        if (!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
         }
         courseService.delete(courseModelOptional.get());
